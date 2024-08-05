a35a76b04d75b0a98909521fe9c10505025ecb6b
airmjay <abdulmojeedaliyu@gmail.com>
2024-08-05 01:59:12 +0100

    add some changes to first file code format

--- first/app/app.php
+++ first/app/app.php
@@ -1,78 +1,69 @@
 <?php
-declare(strict_types=1);
+declare (strict_types = 1);
 
 function getTransactionFile(string $dir_path): array
 {
-     $files = [];
-     foreach(scandir($dir_path) as $file)
-     {
-        if(is_dir($file))
-        {
+    $files = [];
+    foreach (scandir($dir_path) as $file) {
+        if (is_dir($file)) {
             continue;
         }
-        $files[] = $dir_path.$file;
-        
-     }
-     return $files;
+        $files[] = $dir_path . $file;
+
+    }
+    return $files;
 }
-function getTransactions($fileName, ?callable $bank = null)
+function getTransactions($fileName,  ? callable $bank = null)
 {
-    if(! file_exists($fileName))
-    {
-        trigger_error("file ". $fileName. " does not exist ", E_USER_ERROR);
+    if (!file_exists($fileName)) {
+        trigger_error("file " . $fileName . " does not exist ", E_USER_ERROR);
     }
     $file = fopen($fileName, 'r');
     fgetcsv($file);
     $transactions = [];
-    
-    while(($transaction = fgetcsv($file)) !== false)
-    {
-        if($bank !== false)
-        {
-        $transaction = $bank($transaction);
+
+    while (($transaction = fgetcsv($file)) !== false) {
+        if ($bank !== false) {
+            $transaction = $bank($transaction);
         }
         $transactions[] = $transaction;
     }
-    
+
     return $transactions;
 }
 function extractTransaction($transaction)
 {
-    [$date, $check, $description , $amount] = $transaction;
-     $amount = str_replace([',','$'],'', $amount);
-     (float) $amount;
-    return 
-    [
+    [$date, $check, $description, $amount] = $transaction;
+    $amount = str_replace([',', '$'], '', $amount);
+    (float) $amount;
+    return
+        [
         'date' => $date,
         'check' => $check,
         'description' => $description,
-        'amount' => $amount
+        'amount' => $amount,
     ];
 }
 function calculateTotal($transaction)
-{   
+{
     $total = ['NetTotal' => 0, 'Expense' => 0, 'NetIncome' => 0];
-    foreach($transaction as $transaction)
-    {
-        $total['NetTotal'] +=  $transaction['amount'];
-    
-    if($transaction['amount'] > 0)
-    {
-        $total['NetIncome'] += $transaction['amount'];
-    }else
-    {
-        $total['Expense'] += $transaction['amount'];
+    foreach ($transaction as $transaction) {
+        $total['NetTotal'] += $transaction['amount'];
+
+        if ($transaction['amount'] > 0) {
+            $total['NetIncome'] += $transaction['amount'];
+        } else {
+            $total['Expense'] += $transaction['amount'];
+        }
     }
-    }
-  return $total;
+    return $total;
 }
-function formatAmount(float $amount):string
+function formatAmount(float $amount) : string
 {
     $negletive = $amount < 0;
-    return($negletive ? '-' : '') . "$" . number_format(abs($amount),2);
+    return ($negletive ? '-' : '') . "$" . number_format(abs($amount), 2);
 }
-function formatDate(string $date):string
+function formatDate(string $date): string
 {
     return date('m/Y/D ', strtotime($date));
 }
-?>
--- first/view/view.php
+++ first/view/view.php
@@ -1,65 +1,66 @@
 <style>
-    th,table,tr
-    {
-        border:2px solid steelblue;
-        border-collapse:collapse;
-    }
-    table
-    {
-        border-radius: 10px !important;
-    }
-    td
-    {
-        padding: 10px;
-    }
-    tr:nth-child(even) 
-    {
-        /* color: rgb(100, 221, 70); */
-        /* background: steelblue; */
-    }
-    tr:hover 
-    {
-        background:rgb(214, 210, 210);
-    }
-    tr
-    {
-        cursor: pointer;
-    }
+th,
+table,
+tr {
+    border: 2px solid steelblue;
+    border-collapse: collapse;
+}
+
+table {
+    border-radius: 10px !important;
+}
+
+td {
+    padding: 10px;
+}
+
+tr:nth-child(even) {
+    /* color: rgb(100, 221, 70); */
+    /* background: steelblue; */
+}
+
+tr:hover {
+    background: rgb(214, 210, 210);
+}
+
+tr {
+    cursor: pointer;
+}
 </style>
 <table>
-<tr>
-    <td>Date</td>
-    <td>Cheque Number</td>
-    <td>Description</td>
-    <td>Amout</td>
-</tr>
-    
-<?php if(!empty($transactions)): ?>
-<?php foreach($transactions as $transaction):?>
     <tr>
-    <td><?= formatDate($transaction['date'])?></td>
-    <td><?= $transaction['check']?></td>
-    <td><?= $transaction['description']?></td>
-    <?php if($transaction['amount'] > 0): ?>
-    <td style="color:green"><?= formatAmount($transaction['amount'])?></td>
-    <?php elseif($transaction['amount'] < 0): ?>
-    <td style="color:red"><?= formatAmount($transaction['amount'])?></td>
-    <?php else: ?>
-    <td><?= formatAmount($transaction['amount'])?></td>
-    <?php endif ?>
+        <td>Date</td>
+        <td>Cheque Number</td>
+        <td>Description</td>
+        <td>Amout</td>
     </tr>
-<?php endforeach ?>
-   <tr>
-    <td colspan='3' style='text-align:right;'>Net Total:</td>
-    <td> <?= formatAmount($total['NetTotal'])?></td>
-   </tr>
-   <tr>
-   <td colspan='3' style='text-align:right;'>Net Income:</td>
-   <td> <?=formatAmount($total['NetIncome'])?></td>
-   </tr>
-   <tr>
-   <td  colspan='3' style='text-align:right;'>Net Expense:</td>
-   <td style="color:red"><?=formatAmount($total['Expense'])?></td>
-   </tr>
-<?php endif?>
+
+    <?php if(!empty($transactions)): ?>
+    <?php foreach($transactions as $transaction):?>
+    <tr>
+        <td><?= formatDate($transaction['date'])?></td>
+        <td><?= $transaction['check']?></td>
+        <td><?= $transaction['description']?></td>
+        <?php if($transaction['amount'] > 0): ?>
+        <td style="color:green"><?= formatAmount($transaction['amount'])?></td>
+        <?php elseif($transaction['amount'] < 0): ?>
+        <td style="color:red"><?= formatAmount($transaction['amount'])?></td>
+        <?php else: ?>
+        <td><?= formatAmount($transaction['amount'])?></td>
+        <?php endif ?>
+    </tr>
+    <?php endforeach ?>
+    <tr>
+        <td colspan='3' style='text-align:right;'>Net Total:</td>
+        <td> <?= formatAmount($total['NetTotal'])?></td>
+    </tr>
+    <tr>
+        <td colspan='3' style='text-align:right;'>Net Income:</td>
+        <td> <?=formatAmount($total['NetIncome'])?></td>
+    </tr>
+    <tr>
+        <td colspan='3' style='text-align:right;'>Net Expense:</td>
+        <td style="color:red"><?=formatAmount($total['Expense'])?></td>
+    </tr>
+    <?php endif?>
 </table>
